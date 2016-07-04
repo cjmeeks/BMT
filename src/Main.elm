94470e69387b796c03f6ea9320817e809f7c8814
Cj Meeks <cliffmeeks14@gmail.com>
2016-07-04 14:30:26 -0500

    added custom styles update not working

--- src/Main.elm
+++ src/Main.elm
@@ -27,6 +27,10 @@ type alias Model =
     , weight : String
     , battery : String
     , signal : String
+    , something : String
+    , x : String
+    , y : String
+    , z : String
     }
 
 
@@ -132,9 +136,13 @@ highTemp model =
     , time = model.time
     , timeZone = model.timeZone
     , temp = "112.3"
-    , weight = "1.00"
-    , battery = "3.9"
-    , signal = "15"
+    , weight = model.weight
+    , battery = model.battery
+    , signal = model.signal
+    , something = model.something
+    , x = model.x
+    , y = model.y
+    , z = model.z
     }
 
 
@@ -146,10 +154,14 @@ zeroWeight model =
     , date = model.date
     , time = model.time
     , timeZone = model.timeZone
-    , temp = "98"
+    , temp = model.temp
     , weight = "0"
-    , battery = "3.9"
-    , signal = "15"
+    , battery = model.battery
+    , signal = model.signal
+    , something = model.something
+    , x = model.x
+    , y = model.y
+    , z = model.z
     }
 
 
@@ -161,10 +173,14 @@ lowBattery model =
     , date = model.date
     , time = model.time
     , timeZone = model.timeZone
-    , temp = "98"
-    , weight = "1.00"
-    , battery = "3.3"
-    , signal = "15"
+    , temp = model.temp
+    , weight = model.weight
+    , battery = "3.1"
+    , signal = model.signal
+    , something = model.something
+    , x = model.x
+    , y = model.y
+    , z = model.z
     }
 
 
@@ -176,10 +192,14 @@ lowSignal model =
     , date = model.date
     , time = model.time
     , timeZone = model.timeZone
-    , temp = "98"
-    , weight = "1.00"
-    , battery = "3.9"
+    , temp = model.temp
+    , weight = model.weight
+    , battery = model.battery
+    , something = model.something
     , signal = "2"
+    , x = model.x
+    , y = model.y
+    , z = model.z
     }
 
 
@@ -218,7 +238,7 @@ view model =
         dateView =
             div [ class "col-md-4 text-center" ] [ text "Date", div [] [ input [ onInput Date ] [] ] ]
         timeView =
-            div [ class "col-md-4 text-center" ] [ text "Time", div [] [ input [ onInput Time ] [] ] ]
+            div [ class "col-md-4 text-center" ] [ text "Time of Day(hh:mm)", div [] [ input [ onInput Time ] [] ] ]
         zoneView =
             div [ class "col-md-4 text-center" ] [ text "TimeZone", div [] [input [ onInput TimeZone ][] ] ]
         tempView =
@@ -233,15 +253,21 @@ view model =
             div [ class "col-md-4 text-center"]  [ text "BR", div [] [input [onInput Something ] []] ]
     in
         div [ class "container" ]
-            [ div [class "container col-md-9"] [div [ class "row" ] [ firmView, meidView, seqView ]
+            [ div [class "row h1 text-center"] [text "SMRxT Bottle Message Tool"], div [class "container col-md-9"] [div [ class "row" ] [ firmView, meidView, seqView ]
               , div [ class "row" ] [ dateView, timeView, zoneView ]
             , div [class "row " ] [ tempView, weightView, batteryView ]
             , div [ class "row " ] [ signalView ]
-            , div [class "h1"] [ text (String.concat [ model.firmware, ",", model.meid, ",", model.seqNum, ";", model.date, ",", model.time, ",", model.timeZone, ",", model.temp, ",", model.weight, ",", model.battery, ",", model.signal ]) ]
-            ], div [class "container col-md-3"] [div []
-                [ div [class "row"] [button [ class "btn-lg btn-primary btn-block", onClick LowSig ] [ text "Low Signal" ]]
-                , div [class "row"] [button [ class "btn-lg btn-primary btn-block", onClick LowBat ] [ text "Low Battery" ]]
-                , div [class "row"] [button [ class "btn-lg btn-primary btn-block", onClick ZeroWeight ] [ text "Zero Weight" ]]
-                , div [class "row"] [button [ class "btn-lg btn-primary btn-block", onClick HighTemp ] [ text "High Temperature" ]]
-                , div [class "row"] [button [ class "btn-lg btn-danger btn-block", onClick Clear ] [ text "Clear" ]]
-                ]]]
+            , bottleMessageView ]
+            , buttonsView ]
+
+buttonsView : Html Msg
+buttonsView =  div [class "container col-md-3"] [div []
+    [ div [class "row"] [button [ class "btn-lg btn-primary btn-block", onClick LowSig ] [ text "Low Signal" ]]
+    , div [class "row"] [button [ class "btn-lg btn-primary btn-block", onClick LowBat ] [ text "Low Battery" ]]
+    , div [class "row"] [button [ class "btn-lg btn-primary btn-block", onClick ZeroWeight ] [ text "Zero Weight" ]]
+    , div [class "row"] [button [ class "btn-lg btn-primary btn-block", onClick HighTemp ] [ text "High Temperature" ]]
+    , div [class "row"] [button [ class "btn-lg btn-danger btn-block", onClick Clear ] [ text "Clear" ]]
+    ]]
+
+bottleMessageView : Html Msg
+bottleMessageView = div [class "h1"] [ text (String.concat [ model.firmware, ",", model.meid, ",", model.seqNum, ";", model.date, "-", model.time, ",", model.timeZone, ",", model.temp, ",", model.weight, ",", model.battery, ",", model.signal, ",", model.x, ",", model.y, ",", model.z ]) ]
